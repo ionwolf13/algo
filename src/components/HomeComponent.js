c02749720d2c9bf2960387e743a433f8c464b733
jesus_garcia <voli.scorp13@gmail.com>
2021-06-29 13:35:50 -0500

    new

---
+++ src/components/HomeComponent.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {NavComponent} from './NavComponent';
+
+const HomeComponent = () => {
+
+    return(
+        <div>
+            <h1>
+                Algorithms
+            </h1>
+            <NavComponent/>
+        </div>
+    )
+}
+
+export default HomeComponent;
---
+++ src/components/NavComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const NavComponent = () => {
+
+    return(
+        <div>
+            <h1>nav components</h1>
+        </div>
+    )
+}
